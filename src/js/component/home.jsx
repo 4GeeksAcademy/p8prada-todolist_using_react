8299f62b42eabef8ab68d1afab015c861e707ea9
Julian Prada <p8prada@msn.com>
2023-06-27 01:08:11 +0000

    working...

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [userInput, setUserInput] = useState([]);
+	const [taskList, setTaskList] = useState([]);
+
+	const userInputHandler = (event) => {
+		const userInput = event.target.value;
+		console.log(userInput);
+		setUserInput(userInput);
+	}
+
+	const addTask = () => {
+		
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<div className="title">
+				To-Do
+			</div>
+
+			<div className="toDoList">
+				<input onKeyUp={e => e.key === "Enter" && setTaskList((taskList) => {
+
+					return [...taskList, e.target.value]
+				})}/>
+			</div>
+
+			{taskList.map((task, index) => <p key={index}>{task}</p>)}
 		</div>
 	);
 };
